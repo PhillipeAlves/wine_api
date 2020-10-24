e7645c6374c0bd9408a469d482f1dcc5d8c75d3c
PhillipeAlves <phillipe.alves@gmail.com>
2020-10-24 19:04:45 +1100

    search working properly

--- helpers/liveSearch.js
+++ helpers/liveSearch.js
@@ -1,7 +1,7 @@
 const liveSearch = (data, query) => {
   return data.filter((record) => {
     for (let _ in record) {
-      if (record.description) {
+      if (record.description && query) {
         return record.lotCode.includes(query.toUpperCase()) ||
           record.description.includes(query)
           ? record
