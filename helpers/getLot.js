650cb8b6e9c72bd408d38bdbf53e8e4ed520b6d2
PhillipeAlves <phillipe.alves@gmail.com>
2020-10-29 12:46:29 +1100

    refactored getLot helper

--- helpers/getLot.js
+++ helpers/getLot.js
@@ -1,57 +1,60 @@
 const fs = require("fs");
 
-const getLotByCode = (search, type) => {
-  // ===BREAKDOWN TYPE===
-  const breakdownType = type.length === 1 ? type[0] : `${type[0]} & ${type[1]}`;
+const getDataFromFiles = (search, type) => {
 
-  // ===BREAKDOWN DATA===
-  const data = fs.readFileSync(`./public/data/${search}.json`, "utf8");
-  const parsedData = JSON.parse(data);
-  
-  // ===CREATE OBJECT FROM DATA===
-  const wines = parsedData.components.map((wine) => {
-    return {
-      percentage: wine.percentage,
-      key:
-        type.length === 1
-          ? wine[type[0]]
-          : { [type[0]]: wine[type[0]], [type[1]]: wine[type[1]] },
-    };
-  });
+    // ===BREAKDOWN TYPE===
+    const breakdownType = type.length === 1 ? type[0] : `${type[0]} & ${type[1]}`;
 
-  // ===GET SEARCH TYPE===
+    // ===BREAKDOWN DATA===
+    const data = fs.readFileSync(`./public/data/${search}.json`, "utf8");
+    const parsedData = JSON.parse(data);
+    
+    // ===CREATE OBJECT FROM DATA===
+    const wines = parsedData.components.map((wine) => {
+      return {
+        percentage: wine.percentage,
+        key:
+          type.length === 1
+            ? wine[type[0]]
+            : { [type[0]]: wine[type[0]], [type[1]]: wine[type[1]] },
+      };
+    });
+
+    return { wines, breakdownType }
+}
+
+const getResults = (wines, type) => {
   const isSingleValue = type.length === 1 ? true : false
-
-  // ===CREATE ARRAY WITH RESULTS===
-  const getResults = () => {
-    let results = [];
-    let match;
-    wines.filter((wine) => {
-      if (isSingleValue) {
-        match = results.findIndex(result => (result.key === wine.key));
-        if (match <= -1) {
-          results.push(wine);
-        } else {
-          results.forEach(result => (result.key === wine.key) ? result.percentage += wine.percentage : '')
-        }
+  let results = [];
+  let match;
+  wines.forEach( wine => {
+    if (isSingleValue) {
+      match = results.findIndex(result => (result.key === wine.key));
+      if (match <= -1) {
+        results.push(wine);
       } else {
-        match = results.findIndex(result => (result.key.year === wine.key.year && result.key.variety === wine.key.variety));
-        if (match <= -1) {
-          results.push(wine);
-        } else {
-          results.forEach(result => (result.key.year === wine.key.year && result.key.variety === wine.key.variety) ? result.percentage += wine.percentage : '')
-        }
-      }    
-    })
-    return results
-  }
-
-  const finalResults = getResults()
-
-  // ===SORT FROM HIGHEST TO LOWEST===
-  const breakdown = finalResults.sort((a, b) => {
+        results.forEach(result => (result.key === wine.key) ? result.percentage += wine.percentage : '')
+      }
+    } else {
+      match = results.findIndex(result => (result.key.year === wine.key.year && result.key.variety === wine.key.variety));
+      if (match <= -1) {
+        results.push(wine);
+      } else {
+        results.forEach(result => (result.key.year === wine.key.year && result.key.variety === wine.key.variety) ? result.percentage += wine.percentage : '')
+      }
+    }    
+  })
+   // ===SORT FROM HIGHEST TO LOWEST===
+  return results.sort((a, b) => {
     return parseFloat(b.percentage) - parseFloat(a.percentage);
   });
+}
+
+const getLotByCode = (search, type) => {
+
+  const { wines, breakdownType } = getDataFromFiles(search, type)
+
+  const breakdown = getResults(wines, type)
 
   return {
     breakDownType: breakdownType,
